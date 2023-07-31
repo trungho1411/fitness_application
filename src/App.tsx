1b852a502c866af4c7840197fa61c74ede92c1c4
Ho Trung <khanhtrung14111999@gmail.com>
2023-07-31 22:49:05 +0300

    tailwind installation and setup, create navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-
+import Navbar from "@/scenes/navbar"
 
 function App() {
 
   return (
-    <div className='app'>app
+    <div className='app bg-gray-20'>
+      <Navbar />
     </div>
   )
 }
